fdb1c5d65ef30aca996c5b24b0d2847baab7e27d
Il Harper <afanyiyu@hotmail.com>
2021-05-19 15:11:34 +0800

    feat: Add Sample Project

---
+++ samples/SearchSharpIntro/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using SearchSharp;
+
+namespace SearchSharpIntro
+{
+    /// <summary>
+    /// The data type.
+    /// </summary>
+    public record MyData
+    {
+        public string MySearchKey;
+        public int MyValue;
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create DataSet
+            List<MyData> myDataSet = new()
+            {
+                new() {MySearchKey = "TheFirstNumber", MyValue = 1},
+                new() {MySearchKey = "TheSecondNumber", MyValue = 2}
+            };
+
+            // Create SearchStorage
+            SearchStorage<MyData> storage = new();
+
+            // Add Items
+            // foreach (MyData data in myDataSet) storage.Add(data.MySearchKey, data);
+            // Or
+            storage.Add(myDataSet, x => x.MySearchKey);
+
+            // Search
+            HashSet<MyData> result = storage.Search("num");
+        }
+    }
+}
